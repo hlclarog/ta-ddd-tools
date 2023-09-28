7eeebafd74675b31b27926e0f54c2b0cd70834b3
Hector Claro <hclaro@transportationamerica.com>
2023-09-28 18:43:33 -0500

    feat: Get the path to the main repository relative to the current directory

--- scripts/git/check_pending_changes.sh
+++ scripts/git/check_pending_changes.sh
@@ -1,15 +1,24 @@
 #!/usr/bin/env bash
 
+# Get the path to the main repository relative to the current directory
+main_repository_path=$(git rev-parse --show-toplevel)
+
+# Check if the path to the main repository is null (not in a Git repository)
+if [ -z "$main_repository_path" ]; then
+  echo "You are not in a Git repository."
+  exit 1
+fi
+
 # Variable to indicate if there are pending changes
 changes_pending=false
 
-# Get the list of local branches with pending changes
-branches_with_changes=$(git for-each-ref --format '%(refname:short)' --sort -committerdate refs/heads/)
+# Get the list of local branches with pending changes in the main repository
+branches_with_changes=$(git -C "$main_repository_path" for-each-ref --format '%(refname:short)' --sort -committerdate refs/heads/)
 
 # Iterate over each branch and check if it has pending changes
 for branch in $branches_with_changes; do
   # Check if the branch has pending changes
-  changes=$(git log origin/${branch}..${branch})
+  changes=$(git -C "$main_repository_path" log origin/${branch}..${branch})
 
   if [ -n "$changes" ]; then
     echo "The branch '${branch}' has pending changes to push."
